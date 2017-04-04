1b3e7f82f547de9c9879dcc7bec43a8917814615
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-04-05 01:16:54 +0200

    rm fallback

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 module.exports = nanotask
 
 function nanotask () {
-  if (!document.MutationObserver) {
-    return function (cb) {
-      window.requestAnimationFrame(cb, 0)
-    }
-  }
-
   var node = document.createTextNode('')
   var queue = []
   var i = 0
 
-  new document.MutationObserver(function () {
+  new window.MutationObserver(function () {
     for (var i = 0; i < queue.length; i++) queue[i]()
     queue = []
   }).observe(node, { characterData: true })
 
   return function (fn) {
     queue.push(fn)
-    node.data = i++
+    node.data = (i = ++i % 2)
   }
 }
